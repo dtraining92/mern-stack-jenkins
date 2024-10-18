c62409f52683ee3087fb3b2c6e60629726738335
dtraining92 <dtraining92@gmail.com>
2024-10-18 22:51:34 +0530

    Create variable.tf

---
+++ terraform/variables/variable.tf
@@ -0,0 +1,16 @@
+variable "location"{
+    type = string
+    description = "region created"
+}
+variable "ami_id"{
+    type = string
+    description = "ami_id created"
+}
+variable "instance_type"{
+    type = string
+    description = "instance_type provided"
+}
+variable "tag"{
+    type = string
+    description = "tags provided"
+}
